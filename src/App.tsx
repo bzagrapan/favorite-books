cf9bdcad7ffb808b3f8df1c43fa2da921599d3f5
Branislav Zagrapan <branislav.zagrapan@gmail.com>
2024-01-24 15:22:33 +0100

    added new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import './App.css';
-import CustomButton from './components/atoms/Button/Button';
-import CustomInput from './components/atoms/Input/Input';
+import './App.scss';
 import AddBook from './components/molecules/AddBook/AddBook';
-import SavedBook from './components/organisms/SavedBook/SavedBook';
+import SavedBook from './components/molecules/SavedBook/SavedBook';
 import { ISavedBook } from './models/book';
 
 function App() {
@@ -14,24 +12,8 @@ function App() {
     console.log(newBook);
   };
 
-  //TODO tests
   return (
     <div className="App">
-      {/* <CustomButton text="Uložiť" />
-      <div style={{ margin: '20px' }}></div>
-      <CustomInput
-        placeholder="test haha"
-        onInputChange={handleInputChange}
-        maxChars={10}
-      />
-      <div style={{ margin: '20px' }}></div>
-      <CustomInput
-        placeholder="test haha"
-        onInputChange={handleInputChange}
-        maxChars={10}
-        displayWarning={true}
-        warningText="*Povinne pole"
-      /> */}
       <AddBook onBookSave={(newBook: ISavedBook) => handleBookSave(newBook)} />
       <div style={{ margin: '20px' }}></div>
       <SavedBook title="Pietro Mascarpone a asdf sdfdsf sdfds fd sd" />
---
+++ src/components/organisms/BookList/BookList.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Button.scss';
+import { ISavedBook } from '../../../models/book';
+import SavedBook from '../../molecules/SavedBook/SavedBook';
+
+export interface BookListProps {
+  books: ISavedBook[];
+}
+
+const BookList: React.FC<BookListProps> = (props) => {
+  const { books } = props;
+
+  return (
+    <div className="book-list">
+      {books.map((book: ISavedBook, index: number) => (
+        <SavedBook {...book} key={index} />
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
